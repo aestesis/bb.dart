a52ac95347c8cf8d62acbb508f2738912f68af59
renan jegouzo <renan@aestesis.org>
2026-01-22 02:33:40 +0100

    events

--- lib/bb_dart.dart
+++ lib/bb_dart.dart
@@ -1,8 +1,4 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library;
 
-export 'src/bb_dart_base.dart';
+export 'src/event.dart';
 
-// TODO: Export any libraries intended for clients of this package.
