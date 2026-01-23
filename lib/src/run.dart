d9169ffbda027e7cb82205af7423e83fa2140501
renan jegouzo <renan@aestesis.org>
2026-01-23 14:18:45 +0100

    Run.sleep()

--- lib/src/run.dart
+++ lib/src/run.dart
@@ -5,12 +5,23 @@ import 'extension.dart';
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 class Run {
-  static double get time => DateTime.now().millisecondsSinceEpoch.toDouble() / 1000;
+  static double get time =>
+      DateTime.now().millisecondsSinceEpoch.toDouble() / 1000;
   static Timer periodic(Duration tick, void Function(Timer timer) callback) =>
       Timer.periodic(tick, callback);
   static Timer once(Duration wait, VoidCallback callback) =>
       Timer(wait, callback);
   static Timer now(VoidCallback callback) => Timer(Duration.zero, callback);
+  static Future<void> sleep({
+    Duration duration = const Duration(milliseconds: 1),
+  }) {
+    final c = Completer();
+    Timer(duration, () {
+      c.complete();
+    });
+    return c.future;
+  }
+
   // TODO: add isolate run
 }
 
