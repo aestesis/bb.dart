e2707251003fb9acad9add1f138a982fc7fe66f1
renan jegouzo <renan@aestesis.org>
2026-01-24 03:44:43 +0100

    linear permutations

--- example/bin/example.dart
+++ example/bin/example.dart
@@ -2,12 +2,15 @@ import 'package:bb_dart/bb_dart.dart';
 
 void main(List<String> arguments) async {
   Debug.info('VroOOoooom!!');
-  final t0 = List<int>.generate(10, (i) => i);
+  final t0 = List<int>.generate(3, (i) => i);
   final t = Run.time;
   int count = 0;
+  List<List<int>> stack = [];
   await for (final t in t0.permutations()) {
+    stack.add(t);
     count++;
   }
+  Debug.info(stack);
   Debug.info(
     '$count permutations in ${Duration(seconds: (Run.time - t).toInt()).toHumanString()}',
   );
